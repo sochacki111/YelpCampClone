3e15fba90a175cb42cbe0bab48886e6c48a6e991
Michał Sochacki <michalsochacki111@gmail.com>
2020-03-29 15:44:18 +0200

    Added connection to MongoDB. Added viewing results and inserting to database

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-let campgrounds = [
-    {
-        name: 'Salmon Creek',
-        image:
-            'https://pixabay.com/get/57e6d7454e53ae14f1dc8460cf2934771438dbf85254794c712f7ed29348_340.jpg'
-    },
-    {
-        name: 'Granite Hill',
-        image:
-            'https://pixabay.com/get/55e9d043485baa14f1dc8460cf2934771438dbf85254794c712f7ed29348_340.jpg'
-    },
-    {
-        name: "Moutain Goat 's Rest",
-        image:
-            'https://pixabay.com/get/5fe3dc46425ab10ff3d89975c62b3e7f123ac3e45659744d752a7dd092_340.jpg'
-    }
-];
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,7 +26,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', { campgrounds: campgrounds });
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log('Something went wrong!');
+        } else {
+            res.render('campgrounds', { campgrounds: campgrounds });
+        }
+    });
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -39,8 +41,21 @@ app.post('/campgrounds', (req, res) => {
     let campgroundImage = req.body.image;
     let newCampground = { name: campgroundName, image: campgroundImage };
 
-    // Insert new campground
-    campgrounds.push(newCampground);
+    // Create new Campground and save to Database
+    Campground.create(
+        {
+            name: campgroundName,
+            image: campgroundImage
+        },
+        (err, campground) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Created: \n' + campground);
+            }
+        }
+    );
+    // campgrounds.push(newCampground);
     // Redirect back to campgrounds page
     res.redirect('/campgrounds');
 });
