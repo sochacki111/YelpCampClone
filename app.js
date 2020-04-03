bcbf0924676c875cf7e4381aefe89bb4a365fd57
Michał Sochacki <michalsochacki111@gmail.com>
2020-04-03 23:25:45 +0200

    Added working "Sign up", "Log in" and "Log out" buttons

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 /**
  * CAMPGROUND ROUTES
  */
