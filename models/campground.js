c6a6101065392c6ede939b6a016250f3bc48fb39
Michał Sochacki <michalsochacki111@gmail.com>
2020-04-04 12:43:27 +0200

    Added User to Campground association

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,13 @@ const campgroundSchema = new mongoose.Schema({
     name: String,
     description: String,
     image: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,27 +14,34 @@ router.get('/', (req, res) => {
 });
 
 // CREATE - Add new Campground to DB
-router.post('/', (req, res) => {
-    Campground.create(
-        {
-            name: req.body.name,
-            description: req.body.description,
-            image: req.body.image
-        },
-        (err, campground) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Created: \n' + campground);
-            }
+router.post('/', isLoggedIn, (req, res) => {
+    let name = req.body.name;
+    let description = req.body.description;
+    let image = req.body.image;
+    let author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    let newCampground = {
+        name: name,
+        description: description,
+        image: image,
+        author: author
+    };
+
+    Campground.create(newCampground, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Created: \n' + campground);
         }
-    );
+    });
     // Redirect back to campgrounds page
     res.redirect('/campgrounds');
 });
 
 // NEW - display form to make a new campground
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
@@ -46,5 +53,13 @@ router.get('/:id', (req, res) => {
             res.render('campgrounds/show', { campground: campground });
         });
 });
+
+// Middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
