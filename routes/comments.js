8d45dae03e4ef12fcebf2d0e35cfdaf10d462760
Michał Sochacki <michalsochacki111@gmail.com>
2020-04-05 12:47:13 +0200

    Added deleting functionality

--- routes/comments.js
+++ routes/comments.js
@@ -69,6 +69,17 @@ router.put('/:comment_id', (req, res) => {
     );
 });
 
+// Comment delete
+router.delete('/:comment_id', (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, err => {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
